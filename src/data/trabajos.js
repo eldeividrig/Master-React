aa6231e63bd51e45598b7bd5c180391ba151aa25
david <davidgabrielb@hotmail.com>
2022-07-20 02:55:40 -0300

    subiendo

--- src/data/trabajos.js
+++ src/data/trabajos.js
@@ -4,28 +4,48 @@ export const trabajos = [
         'nombre': 'David Barboza Web',
         'url': 'davidbarbozaweb.com',
         'tecnologias': 'HTML, CSS, JS, PHP, MySQL',
-        'categorias': 'Desarrollo Web'
+        'categorias': 'Desarrollo Web',
+        'descripcion': 'Armando mi portafolio personal'
     },
     {
         'id': 'facebook',
         'nombre': 'Facebook',
         'url': 'facebook.com',
         'tecnologias': 'React, Node, Mongo',
-        'categorias': 'Desarrollo Web'
+        'categorias': 'Desarrollo Web',
+        'descripcion': 'Armando mi portafolio personal'
     },
     {
         'id': 'twitter',
         'nombre': 'David Barboza Web',
         'url': 'twitter.com',
         'tecnologias': 'HTML, CSS',
-        'categorias': 'Maquetación Web'
+        'categorias': 'Maquetación Web',
+        'descripcion': 'Armando mi portafolio personal'
     },
     {
         'id': 'linkedin',
         'nombre': 'Linkedin',
         'url': 'linkedin.COM',
         'tecnologias': 'HTML, CSS, JS, PHP, Laravel',
-        'categorias': 'Desarrollo Web'
+        'categorias': 'Desarrollo Web',
+        'descripcion': 'Armando mi portafolio personal'
     },
+    {
+        'id': 'udemy',
+        'nombre': 'Udemy',
+        'url': 'udemy.com/user/victor-robles-2',
+        'tecnologias': 'MEAN Stack',
+        'categorias': 'Maquetación Web',
+        'descripcion': 'Armando mi portafolio personal'
+    },
+    {
+        'id': 'youtube',
+        'nombre': 'Youtube',
+        'url': 'youtube.com/c/victorroblesWeb',
+        'tecnologias': 'JS, React, PHP, Symfony',
+        'categorias': 'Desarrollo Web',
+        'descripcion': 'Armando mi portafolio personal'
+    }
 
 ];
--- src/router/MisRutas.js
+++ src/router/MisRutas.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Routes, Route, BrowserRouter, NavLink, Navigate } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import { Contacto } from '../components/Contacto';
 import { Curriculum } from '../components/Curriculum';
 import { Inicio } from '../components/Inicio';
 import { Footer } from '../components/layout/Footer';
 import { HeaderNav } from '../components/layout/HeaderNav';
 import { Portafolio } from '../components/Portafolio';
+import { Proyecto } from '../components/Proyecto';
 import { Servicios } from '../components/Servicios';
 
 export const MisRutas = () => {
@@ -23,7 +24,12 @@ export const MisRutas = () => {
           <Route path='/servicios' element={<Servicios />} />
           <Route path='/curriculum' element={<Curriculum />} />
           <Route path='/contacto' element={<Contacto />} />
-
+          <Route path='/proyecto/:id' element={<Proyecto />} />
+          <Route path='*' element={
+            <div className='page'>
+              <h1 className='heading'>Error 404</h1>
+            </div>
+          } />
         </Routes>
       </section>
 
