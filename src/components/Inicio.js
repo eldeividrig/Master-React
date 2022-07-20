d525c888db98765e6cf3243b6266595cf02b055c
David Gabriel Barboza <davidgabrielb@hotmail.com>
2022-07-20 11:49:32 -0300

    terminado

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { ListadoTrabajos } from './ListadoTrabajos'
 
 export const Inicio = () => {
   return (
@@ -8,16 +9,15 @@ export const Inicio = () => {
         Hola, soy <strong>David Barboza</strong> y soy Ingeniero en Informática, 
         y orfezco mis servicios de <strong>programación</strong> y <strong>desarrollo</strong> en todo tipo de  proyectos WEB.
       </h1>
-      <h2>
+      <h2 className='title'>
         Te ayudo a crear tu sitio o aplicaciones Web, 
         tener más visibilidad y relevancia en internet. <Link to="/contacto">Contacta Conmigo</Link>
       </h2>
       <section className='last-works'>
         <h2 className='heading'>Algunos de mis Proyectos</h2>
         <p>Estos son algunos de mis trabajos de desarrollo Web</p>
-        <div className='works'>
-
-        </div>
+        
+        <ListadoTrabajos limite="2"/>
       </section>
     </div>
   )
